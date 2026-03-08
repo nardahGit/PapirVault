c1169f64430f973fdca67bd9ab4a94922e0dcd6c
nardahGit <fiderananardah@gmail.com>
2026-03-08 20:14:00 +0100

    Hide private Firebase config from GitHub

--- App/Main/firebase-env.js
+++
@@ -1,9 +0,0 @@
-window.PAPIRVAULT_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB7as4fsbi6DeQCc9Ae9UXgYZPm_Zal4nY",
-    authDomain: "nardahdev.firebaseapp.com",
-    projectId: "nardahdev",
-    storageBucket: "nardahdev.firebasestorage.app",
-    appId: "1:587661895623:web:598ecc1f088de515929be2"
-};
-
-window.PAPIRVAULT_PUBLIC_VERIFY_BASE_URL = "https://nardahdev.web.app/Body/VerifyStatus.html";
